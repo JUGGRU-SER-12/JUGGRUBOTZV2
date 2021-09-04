1947dd8c605f20ed49d463c544cbe9ad4982bd0a
JUGGRU SER <89448391+JUGGRU-SER-12@users.noreply.github.com>
2021-09-04 22:43:36 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,21 +11,21 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/yCYYyEJ.jpeg";
+  r_text[0] = "https://www.linkpicture.com/q/juggru.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919895828468*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*CREATED BY JUGGRUSER*
+*OWNER NO: wa.me/919656813724*
 
-*bot setting video : https://youtu.be/RlSG4ZgPls0*
+*MY YT CHANNEL : https://www.youtube.com/channel/UC0C4MiybHKd2vB26EylS-5A*
 
-*githublink : https://github.com/cyberchekuthan/Amalser_v2*
+*GITHUB LINK : https://github.com/JUGGRU-SER-12/JUGGRUBOTZV2*
 
-*audio commads : https://github.com/cyberchekuthan/Amalser_v2/tree/master/uploads*
+*AUDIO CMNDS : https://github.com/JUGGRU-SER-12/JUGGRUBOTZV2/tree/master/uploads*
 `}) 
 
 }));
